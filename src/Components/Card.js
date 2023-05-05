0aaa9bf1ba78530b6509ee75a33d2b567a6ef391
dave221b <119088976+dave221b@users.noreply.github.com>
2023-05-05 20:53:05 +0530

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,12 +2,10 @@ import './card.css';
 
 export const Cardcomponent = ({image, title, body}) => {
     return(
-        <>
            <div className='card'>
                 <img src={image} alt='QR code'/>
                 <div className='title'>{title}</div>
                 <div className='body'>{body}</div>
             </div>
-        </>
     )
 }
