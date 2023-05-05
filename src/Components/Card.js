c7314f7f2715275974702739748eb51b47b331b2
Chandu <chandrakanth.3060@gmail.com>
2023-05-05 16:48:25 +0530

    removed card(bootstrap)

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,17 +4,6 @@ import './card.css';
 export const Cardcomponent = ({image, title, body}) => {
     return(
         <>
-            {/* <Card style={{ width: '12rem'}}>
-                <Card.Img variant='top' src={image} />
-                <Card.Body>
-                    <Card.Title>
-                        {title}
-                    </Card.Title>
-                    <Card.Text>
-                        {body}
-                    </Card.Text>
-                </Card.Body>
-            </Card> */}
             <div className='card'>
                 <img src={image} alt='QR code'/>
                 <div className='title'>{title}</div>
