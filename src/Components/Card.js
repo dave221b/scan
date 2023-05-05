da9027cf3d287d6bd8a22a3c15a93ee0f8af0d2e
dave221b <119088976+dave221b@users.noreply.github.com>
2023-05-05 20:52:41 +0530

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,18 +3,7 @@ import './card.css';
 export const Cardcomponent = ({image, title, body}) => {
     return(
         <>
-            {/* <Card style={{ width: '12rem'}}>
-                <Card.Img variant='top' src={image} />
-                <Card.Body>
-                    <Card.Title>
-                        {title}
-                    </Card.Title>
-                    <Card.Text>
-                        {body}
-                    </Card.Text>
-                </Card.Body>
-            </Card> */}
-            <div className='card'>
+           <div className='card'>
                 <img src={image} alt='QR code'/>
                 <div className='title'>{title}</div>
                 <div className='body'>{body}</div>
